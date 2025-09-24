9b38380635aec63a29ba3ebb7d23da6051d08658
ManuelValdivia03 <zs23014054@estudiantes.uv.mx>
2025-09-23 18:50:48 -0600

    Inicio de contruccion de RegistrationWindow

--- WPFTheWeakestRival/RegistrationWindow.xaml.cs
+++ WPFTheWeakestRival/RegistrationWindow.xaml.cs
@@ -1,27 +1,77 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using Microsoft.Win32;
+using WPFTheWeakestRival.Properties.Langs;
+using System;
+using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Net.Mail;
+using System.Security.Cryptography;
+using System.Threading;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
+
 
 namespace WPFTheWeakestRival
 {
-    /// <summary>
-    /// Lógica de interacción para RegistrationWindow.xaml
-    /// </summary>
     public partial class RegistrationWindow : Window
     {
+        private string pickedImagePath;
+        private string savedImagePath;
+
         public RegistrationWindow()
         {
             InitializeComponent();
         }
+
+        private void UpdateLabelColors()
+        {
+            // Esqueleto: implementación eliminada para evitar lógica en este archivo
+        }
+
+        private void ChooseImageClick(object sender, RoutedEventArgs e)
+        {
+            // Esqueleto: implementación eliminada
+        }
+
+        private static string SaveProfileImageCopy(string originalPath)
+        {
+            // Esqueleto: implementación eliminada
+            return null;
+        }
+
+        private static byte[] HashPassword(string password)
+        {
+            // Esqueleto: implementación eliminada
+            return null;
+        }
+
+        private static bool IsValidEmail(string email)
+        {
+            // Esqueleto: implementación eliminada
+            return false;
+        }
+
+        private static bool PasswordMeetsRequirements(string password)
+        {
+            // Esqueleto: implementación eliminada
+            return false;
+        }
+
+        private static bool UsernameHasNoSpaces(string username)
+        {
+            // Esqueleto: implementación eliminada
+            return false;
+        }
+
+        private void RegisterClick(object sender, RoutedEventArgs e)
+        {
+            // Esqueleto: implementación eliminada
+        }
+
+        private void CancelClick(object sender, RoutedEventArgs e)
+        {
+            // Esqueleto: implementación eliminada
+        }
+
     }
 }
